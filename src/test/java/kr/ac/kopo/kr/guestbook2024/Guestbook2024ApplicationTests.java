9a690ca3083667b71f2b585df750641d9832ffd8
00seungho <tmdgh9910@naver.com>
2024-04-18 11:50:47 +0900

    __init__commit

---
+++ src/test/java/kr/ac/kopo/kr/guestbook2024/Guestbook2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.kr.guestbook2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guestbook2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
